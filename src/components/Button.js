5adafcfd7e086c265bd3d8e212424d76eda6d6cc
JJ <jerrica.mj@gmail.com>
2021-02-15 14:19:55 -0800

    Add classes, onClick event, and disabled attributes to button.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,26 @@
 import React from "react";
 
 import "components/Button.scss";
+import { action } from "@storybook/addon-actions/dist/preview";
 
 export default function Button(props) {
+  let buttonClass = "button";
+
+  // Add relevant CSS classes to the button
+  if (props.confirm) {
+    buttonClass += " button--confirm";
+  } else if (props.danger) {
+    buttonClass += " button--danger";
+  }
+
+
   return (
     <>
-      <button>{props.children}</button>
+      <button
+        className={buttonClass}
+        onClick={props.onClick}
+        disabled={props.disabled}
+      >{props.children}
+      </button>
     </>);
 }
