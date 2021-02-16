b6e4be8c34284af1909b244211c313c08a4eba81
JJ <jerrica.mj@gmail.com>
2021-02-15 20:19:16 -0800

    Implement DayList to return an empty ul element for now.

---
+++ src/components/DayList.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import classNames from "classnames";
+
+// import "components/DayList.scss";
+
+export default function DayList(props) {
+
+  return (
+    <ul>
+    </ul>
+  );
+};
