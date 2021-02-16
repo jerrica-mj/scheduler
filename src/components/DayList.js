75a30f67aa7ca7d72930e98e69f3311f5c6107db
JJ <jerrica.mj@gmail.com>
2021-02-15 21:09:58 -0800

    Update component to iterate over an array of days (passed as a prop), and return a DayListItem for each array element/day.

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,12 +1,29 @@
 import React from "react";
 // import classNames from "classnames";
 
+import DayListItem from "components/DayListItem";
+
 // import "components/DayList.scss";
 
 export default function DayList(props) {
+  // iterate over the days array to create a list item for each
+  // check if selected day matched current object/day name
+  // * Remember to use a key prop to uniquely id list items in React
+  const dayListItems = props.days.map(day => {
+    return (
+      <DayListItem
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        selected={day.name === props.day}
+        setDay={props.setDay}
+      />
+    );
+  });
 
   return (
     <ul>
+      {dayListItems}
     </ul>
   );
 };
