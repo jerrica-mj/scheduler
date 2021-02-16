6b176ed5fede915cf35439f91657557e3011ae70
JJ <jerrica.mj@gmail.com>
2021-02-15 17:49:14 -0800

    Build the DayListItem component with props.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,12 +1,19 @@
 import React from "react";
-
+import classNames from "classnames";
 
 export default function DayListItem(props) {
+  const listItemClass = classNames({
+    "item--selected": props.selected
+  });
 
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2>
-      <h3 className="text--light">X spots remaining</h3>
+    <li
+      className={listItemClass}
+      onClick={() => props.setDay(props.name)}
+      disabled={props.spots < 1}
+    >
+      <h2 className="text--regular">{props.name}</h2>
+      <h3 className="text--light">{props.spots} spots remaining</h3>
     </li>
   );
 };
