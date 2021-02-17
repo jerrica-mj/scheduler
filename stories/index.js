32c61b4c1577e7e588a65953d950b396fd26b5eb
JJ <jerrica.mj@gmail.com>
2021-02-16 22:58:15 -0800

    Add Show component story to Appointment stories.

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 
 storiesOf("Button", module)
@@ -152,4 +153,12 @@ storiesOf("Appointment", module)
   ))
   .add("Empty", () => (
     <Empty onAdd={action("onAdd")} />
+  ))
+  .add("Show", () => (
+    <Show
+      student="Lydia Miller-Jones"
+      interviwer={interviewer}
+      onEdit={action("onEdit")}
+      onDelet={action("onDelete")}
+    />
   ));
