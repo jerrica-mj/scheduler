72006cd32182dca8751eab1b144b5c0a94d29e57
JJ <jerrica.mj@gmail.com>
2021-02-16 23:10:59 -0800

    Import the Confirm component and add a story to the Appointment stories.

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 
 storiesOf("Button", module)
@@ -161,4 +162,7 @@ storiesOf("Appointment", module)
       onEdit={action("onEdit")}
       onDelete={action("onDelete")}
     />
+  ))
+  .add("Confirm", () => (
+    <Confirm />
   ));
