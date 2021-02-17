6bb91acd2ef0ea387e011903c033f120e00851dc
JJ <jerrica.mj@gmail.com>
2021-02-16 16:08:40 -0800

    Refactor the component to use prop values.

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -5,10 +5,27 @@ import "components/InterviewerList.scss";
 import InterviewerListItem from "components/InterviewerListItem";
 
 export default function InterviewerList(props) {
+  // iterate over the array and create a li for each interviewer
+  const interviewerItems = props.interviewers.map(interviewer => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={props.setInterviewer}
+      >
+      </InterviewerListItem>
+    );
+  });
+
+
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">
+        {interviewerItems}
+      </ul>
     </section>
   );
 };
