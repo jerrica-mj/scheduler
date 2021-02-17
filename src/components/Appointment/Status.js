c16b9caad1b16d1924be5dd0fdf1add70e329864
JJ <jerrica.mj@gmail.com>
2021-02-16 23:17:54 -0800

    Build the initial Status component with static data.

---
+++ src/components/Appointment/Status.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// Saving and deleting appointments is asynchronous.
+//  We need to indicate to the user that the operation
+//  is in progress --> a status message with CSS animation
+export default function Status(props) {
+  return (
+    <main className="appointment__card appointment__card--status">
+      <img
+        className="appointment__status-image"
+        src="images/status.png"
+        alt="Loading"
+      />
+      <h1 className="text--semi-bold">Deleting</h1>
+    </main>
+  );
+};
