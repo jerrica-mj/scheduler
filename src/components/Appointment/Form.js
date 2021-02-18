6ff836e45b798b620b5e8070eeb724088e942f97
JJ <jerrica.mj@gmail.com>
2021-02-17 22:19:20 -0800

    Update input and InterviewerList elements to controlled components using state hooks.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,9 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
 export default function Form(props) {
+  const [name, setName] = useState(props.name || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -13,13 +16,14 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={props.name}
+            value={name}
+            onChange={(event) => setName(event.target.value)}
             /*
               This must be a controlled component
             */
           />
         </form>
-        <InterviewerList interviewers={props.interviewers} value={props.interviewer} onChange={props.setInterviewer} />
+        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
