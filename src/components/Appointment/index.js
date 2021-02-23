7792c00953c96ec5f70f928443e4b51b4d5b11df
JJ <jerrica.mj@gmail.com>
2021-02-22 17:20:25 -0800

    Import and destructure the custom useVisualMode hook.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,9 +5,19 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from "../../hooks/useVisualMode";
 
 
 export default function Appointment(props) {
+  // the mode constants -- for useVisualMode hook
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+
+  // destructure our useVisualMode hook and use to render components based on mode value
+  const {mode, transition, back} = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
   return (
     <article className="appointment">
       <Header time={props.time}></Header>
