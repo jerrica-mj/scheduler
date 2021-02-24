c2e0acc6b3b8d2254fc992e126dc880cf3f7a997
JJ <jerrica.mj@gmail.com>
2021-02-23 20:28:36 -0800

    Pass the cancelInterview function to the Appointment element as a prop value.

--- src/components/Application.js
+++ src/components/Application.js
@@ -118,6 +118,7 @@ export default function Application(props) {
         interview={interview}
         interviewers={dailyInterviewers}
         bookInterview={bookInterview}
+        cancelInterview={cancelInterview}
       />
     );
   });
