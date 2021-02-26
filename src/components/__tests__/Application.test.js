bddfd27282651beda39c2166265c941ebf3d007f
JJ <jerrica.mj@gmail.com>
2021-02-26 14:50:45 -0800

    Add describe() to group all Application component tests.

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -8,15 +8,20 @@ import { fireEvent } from "@testing-library/react/dist";
 afterEach(cleanup);
 
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  // render (making an async axios request) and check that day is set to Monday, once the app has rendered (wait for Monday element)
-  const {getByText} = render(<Application />);
-  // use waitForElement to return a Promise to make our test async
-  // waitForElement returns a truthy value if the DOM node is found, or rejects if not found after a time out
-  return waitForElement(() => getByText("Monday"))
-  // fire the click event on the 'Tuesday' menu button and verify the text 'Leopold Silvers' is in the document
-    .then(() => {
-      fireEvent.click(getByText("Tuesday"));
-      expect(getByText("Leopold Silvers")).toBeInTheDocument();
-    })
+describe("Application", () => {
+
+  it("defaults to Monday and changes the schedule when a new day is selected", () => {
+    // render (making an async axios request) and check that day is set to Monday, once the app has rendered (wait for Monday element)
+    const {getByText} = render(<Application />);
+    // use waitForElement to return a Promise to make our test async
+    // waitForElement returns a truthy value if the DOM node is found, or rejects if not found after a time out
+    return waitForElement(() => getByText("Monday"))
+    // fire the click event on the 'Tuesday' menu button and verify the text 'Leopold Silvers' is in the document
+      .then(() => {
+        fireEvent.click(getByText("Tuesday"));
+        expect(getByText("Leopold Silvers")).toBeInTheDocument();
+      })
+  });
+
+
 });
