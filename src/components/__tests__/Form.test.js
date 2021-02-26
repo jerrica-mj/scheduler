aad3b40e938d54e38f740a9fbbfebcb87510e0f1
JJ <jerrica.mj@gmail.com>
2021-02-25 22:26:49 -0800

    Removed last 2 tests, as they were the same as the previously added 2 tests.

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -50,7 +50,7 @@ describe("Form", () => {
     );
 
     // click the save button
-      fireEvent.click(getByText("Save"));
+    fireEvent.click(getByText("Save"));
 
     /* 1. validation is shown */
     expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
@@ -88,28 +88,4 @@ describe("Form", () => {
     expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
   });
 
-
-  it("validates that the student name is not blank", () => {
-  /* 1. Create the mock onSave function */
-
-  /* 2. Render the Form with interviewers and the onSave mock function passed as an onSave prop, the name prop should be blank or undefined */
-
-  /* 3. Click the save button */
-
-  expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
-  expect(onSave).not.toHaveBeenCalled();
-});
-
-
-it("calls onSave function when the name is defined", () => {
-  /* 1. Create the mock onSave function */
-
-  /* 2. Render the Form with interviewers, name and the onSave mock function passed as an onSave prop */
-
-  /* 3. Click the save button */
-
-  expect(queryByText(/student name cannot be blank/i)).toBeNull();
-  expect(onSave).toHaveBeenCalledTimes(1);
-  expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
-});
 });
