1c96c0b23dd1a4db3b9bcc9595258f9879064f8c
JJ <jerrica.mj@gmail.com>
2021-02-26 13:48:49 -0800

    Add the test body: render the app, use a promise to wait for the Monday node to render, then chain an click even firing and expectation the check that the schedule has changed.

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -3,13 +3,20 @@ import React from "react";
 import { render, cleanup, waitForElement } from "@testing-library/react";
 
 import Application from "components/Application";
+import { fireEvent } from "@testing-library/react/dist";
 
 afterEach(cleanup);
 
-// it("renders without crashing", () => {
-//   render(<Application />);
-// });
 
 it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  // get days from 'axios', render and check that day is set to Monday
+  // render (making an async axios request) and check that day is set to Monday, once the app has rendered (wait for Monday element)
+  const {getByText} = render(<Application />);
+  // use waitForElement to return a Promise to make our test async
+  // waitForElement returns a truthy value if the DOM node is found, or rejects if not found after a time out
+  return waitForElement(() => getByText("Monday"))
+  // fire the click event on the 'Tuesday' menu button and verify the text 'Leopold Silvers' is in the document
+    .then(() => {
+      fireEvent.click(getByText("Tuesday"));
+      expect(getByText("Leopold Silvers")).toBeInTheDocument();
+    })
 });
