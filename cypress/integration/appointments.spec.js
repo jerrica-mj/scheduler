303328b13ac8c713cfb753cd6fe622da62936310
JJ <jerrica.mj@gmail.com>
2021-02-27 15:59:03 -0800

    Add to the test to click the first Add button.

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -5,8 +5,13 @@ describe("Appointments", () => {
 
   it("should book an interview", () => {
     cy.visit("/");
-    // make sure data is loaded --> use built-in retry of contains
+    // Ensure data is loaded --> built-in retry of contains
     cy.contains("Monday");
+
+    // Click the "Add" button in the second appoitnment
+    cy.get("[alt=Add]")
+      .first() // use first because there is a hidden Add button in the last appointment, and Cypress can only click a single element
+      .click();
   });
 
 
