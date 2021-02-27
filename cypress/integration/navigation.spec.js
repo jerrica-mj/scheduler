119771841ca56b99c831ed300134c146c3279d04
JJ <jerrica.mj@gmail.com>
2021-02-27 00:32:44 -0800

    Add a test to navigate to Tuesday.

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -2,4 +2,12 @@ describe("Navigation", () => {
   it("should visit root", () => {
     cy.visit("/");
   });
+
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+
+    cy.get("li.day-list__item").contains("Tuesday")
+      .click();
+  });
 });
