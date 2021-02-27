1e6e66d4dfc674ec8ef950435a9cbacda6667189
JJ <jerrica.mj@gmail.com>
2021-02-26 16:31:18 -0800

    Render the application component; use async/await to wait for the appointment with 'Archie Cohen' to render, then log the pretty HTML version of the app container.

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement } from "@testing-library/react";
+import { render, cleanup, waitForElement, getByText, prettyDOM } from "@testing-library/react";
 
 import Application from "components/Application";
 import { fireEvent } from "@testing-library/react/dist";
@@ -23,5 +23,34 @@ describe("Application", () => {
       })
   });
 
+
+  it("loads data, books an interview, and reduces the spots remaining for the first day by 1", async () => {
+    // Render the Application--getByText() will be used from import
+    const {container} = render(<Application />);
+    // console.log(prettyDOM(container));
+
+    // Wait until "Archie Cohen" is displayed
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    console.log(prettyDOM(container));
+
+    // Click the "Add" button on the first empty appointment
+      // .then(() => {
+      //   fireEvent.click()
+      // })
+
+    // Enter "Lydia Miller-Jones" into the input with placeholder "Enter Student Name"
+
+    // Click the first interviewer in the list
+
+    // CLick the "Save" buton
+
+    // Check that the element with "Saving" is displayed
+
+    // Wait until the element with "Lydia Miller-Jones" is displayed
+
+    // Check that the "Monday" DayListItem is displayed and has the text "no spots remaining"
+
+  });
 
 });
